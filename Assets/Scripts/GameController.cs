76221ffad866ee8b8202c0b7e9da05243c9761dc
YasutakaAwano <eewano@gmail.com>
2016-03-01 10:39:48 +0900

    Revert "1.スクリプトを大幅変更中。"
    This reverts commit 281f0f956f580b5a5f9670df2f7e34602532fa80.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,8 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 
+	enum State {PLAYING, GAMEOVER}
+	private State state;
+
+	[SerializeField] private PlayerController player;
 	[SerializeField] private Text GameIsOver;
-	[SerializeField] private Text scoreLabel;
+	[SerializeField] private Text score01Label;
+	[SerializeField] private Text score02Label;
 	[SerializeField] private Text TapToTitle;
 	[SerializeField] private GameObject ButtonLeft;
 	[SerializeField] private GameObject ButtonRight;
@@ -14,59 +19,123 @@ public class GameController : MonoBehaviour {
 
 	private AudioSource stageBGM;
 	private StageSoundEffect stageSoundEffect;
-    private PlayerController playerController;
-
-    void Awake()
-    {
-        stageBGM = GameObject.Find("Main Camera").GetComponent<AudioSource>();
-        stageSoundEffect = GameObject.Find("StageSoundEffect").GetComponent<StageSoundEffect>();
-        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
-    }
 
 	void Start()
 	{
-        GameIsOver.enabled = false;
-        TapToTitle.enabled = false;
-
-		StartCoroutine(GamePlaying());
+		stageBGM = GameObject.Find("Main Camera").GetComponent<AudioSource>();
+		stageSoundEffect = GameObject.Find("StageSoundEffect").GetComponent<StageSoundEffect>();
+		
+		Playing ();
 	}
 
 	void Update()
-    {
-        if(playerController.Life() <= 0) {
-            if(Input.GetMouseButtonDown(0)) {
-				SceneManager.LoadScene("Title");
+	{
+		switch (state) {
+
+		case State.PLAYING:
+			//-----NORMAL STAGE のスコアラベルを更新する-----
+			if (TitleController.Stage01) {
+				int score01 = CalcScoreStage01 ();
+				score01Label.text = "Score : " + score01 + "pts";
+				if (player.Life () <= 0) {
+					//ここで1度GameController.csを無効にしないと、ゲームオーバーBGMが重複し続けてしまう
+					//理由は現時点で解決出来ていない
+					enabled = false;
+					if (PlayerPrefs.GetInt ("Hiscore01") < score01) {
+						PlayerPrefs.SetInt ("Hiscore01", score01);	//NORMAL STAGE のハイスコアを更新する
+					}
+					Invoke ("GameOver", 0.5f);
+				}
 			}
-        }
+			//----------
+
+			//-----HARD STAGE のスコアラベルを更新する-----
+			else if(TitleController.Stage02) {
+				int score02 = CalcScoreStage02 ();
+				score02Label.text = "Score : " + score02 + "pts";
+				if (player.Life () <= 0) {
+					//ここで1度GameController.csを無効にしないと、ゲームオーバーBGMが重複し続けてしまう
+					//理由は現時点で解決出来ていない
+					enabled = false;
+					if (PlayerPrefs.GetInt ("Hiscore02") < score02) {
+						PlayerPrefs.SetInt ("Hiscore02", score02);	//HARD STAGE のハイスコアを更新する
+					}
+					Invoke ("GameOver", 0.5f);
+				}
+			}
+			//----------
+
+			break;
+
+		case State.GAMEOVER:
+			if(Input.GetMouseButtonDown(0))
+				SceneManager.LoadScene("Title");
+			break;
+		}
 	}
 
-    IEnumerator GamePlaying() {
+	//-----まずはすべてのテキストやボタンを非表示にしてから、各ステートで表示させたいものをtrueにしている-----
+	void AllFalse()
+	{
+		GameIsOver.enabled = false;
+		score01Label.enabled = false;
+		score02Label.enabled = false;
+		TapToTitle.enabled = false;
 
+		ButtonLeft.gameObject.SetActive(false);
+		ButtonRight.gameObject.SetActive(false);
+		ButtonJump.gameObject.SetActive(false);
+	}
+	//----------
 
+	void Playing()
+	{
+		state = State.PLAYING;
+		AllFalse ();
 
-		while (true) {
-            int score = CalcScore();
-            scoreLabel.text = "Score : " + score + "pts";
+		if (TitleController.Stage01) {
+			score01Label.enabled = true;
+		} else if (TitleController.Stage02) {
+			score02Label.enabled = true;
+		}
 
-		    if (playerController.Life() <= 0)
-		    {
-			    if (PlayerPrefs.GetInt("Hicore") < score) {
-				    PlayerPrefs.SetInt("Hiscore", score);
-			    }
-			    yield return new WaitForSeconds(0.5f);
-                Debug.Log("GAMEOVER");
-			    stageBGM.Stop();
-			    stageSoundEffect.GameIsOver();
-                GameIsOver.enabled = true;
-                TapToTitle.enabled = true;
-			    break;
-		    }
-        }
+		ButtonLeft.gameObject.SetActive(true);
+		ButtonRight.gameObject.SetActive(true);
+		ButtonJump.gameObject.SetActive(true);
+	}
+
+	void GameOver()
+	{
+		state = State.GAMEOVER;
+		AllFalse ();
+		//PLAYINGステートでGameController.csを1度無効にしているので、
+		//ゲームオーバー時に再度有効にしている
+		enabled = true;
+
+		if (TitleController.Stage01) {
+			score01Label.enabled = true;
+		} else if (TitleController.Stage02) {
+			score02Label.enabled = true;
+		}
+
+		GameIsOver.enabled = true;
+		TapToTitle.enabled = true;
+
+		stageBGM.Stop();
+		stageSoundEffect.GameIsOver ();
+
+		//PlayerPrefs.DeleteAll();	//ハイスコアを初期化する
 	}
 
 	//-----プレイヤーの走行距離をスコアとする-----
-	int CalcScore()
+	int CalcScoreStage01()
 	{
-		return(int)playerController.transform.position.z;
+		return(int)player.transform.position.z;	//NORMAL STAGE
 	}
+
+	int CalcScoreStage02()
+	{
+		return(int)player.transform.position.z;	//HARD STAGE
+	}
+	//----------
 }
