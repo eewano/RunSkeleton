5029b6dba4cfd7da064b1278f20200520c99c9bc
YasutakaAwano <eewano@gmail.com>
2016-07-11 18:29:57 +0900

    -Eventによるシステム処理に移行- 1: キー操作によるプレイヤーの移動のスクリプトを作成中。

---
+++ Assets/Mgr_PlayerKeyCtrl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class Mgr_PlayerKeyCtrl : MonoBehaviour {
+
+    [SerializeField]
+    private float keySpeed;
+
+    private bool keyCtrl;
+
+    void Start() {
+        keyCtrl = true;
+    }
+
+    void Update() {
+        if (keyCtrl == true)
+        {
+            float translation = Input.GetAxis("Horizontal") * keySpeed;
+            translation *= Time.deltaTime;
+            transform.Translate(translation, 0, 0);
+        }
+    }
+
+    public void DisableKeyCtrl(object o, EventArgs e) {
+        keyCtrl = false;
+    }
+}
---
+++ Assets/Mgr_PlayerKeyJump.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mgr_PlayerKeyJump : MonoBehaviour {
+
+    [SerializeField]
+    private float gravity;
+    [SerializeField]
+    private float jumpSpeed;
+    private CharacterController charaCtrl;
+    private Animator jumpAnimator;
+    private Vector3 moveDirection = Vector3.zero;
+
+    private bool keyJump = false;
+
+    void Awake() {
+        charaCtrl = GetComponent<CharacterController>();
+        jumpAnimator = GetComponent<Animator>();
+    }
+
+    void Update() {
+        if (charaCtrl.isGrounded)
+        {
+            moveDirection.y = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            keyJump = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            keyJump = false;
+        }
+
+
+        if (keyJump == true)
+        {
+            JumpMode();
+        }
+    }
+
+    void JumpMode() {
+        if (charaCtrl.isGrounded && keyJump == true) {
+            moveDirection.y = jumpSpeed;
+            jumpAnimator.SetTrigger("Jump");
+        }
+    }
+}
