ccff4a5b01002decbb4c2c50a9dae8cbf51091d9
YasutakaAwano <eewano@gmail.com>
2016-09-19 21:26:52 +0900

    1: NORMAL面のエリア数を増加。及び一部のエリアを修正。 2: HARD面のエリア数を増加。一部のエリアを修正中。

---
+++ Assets/Scripts/GameScripts/BallMove02.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class BallMove02 : MonoBehaviour {
+
+    void Start() {
+        gameObject.SetActive(false);
+    }
+
+    public void BallAppear(object o, EventArgs e) {
+        gameObject.SetActive(true);
+    }
+
+    void OnTriggerEnter(Collider col) {
+        if(col.gameObject.tag == "Ball02StandBy")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GameScripts/BallStandBy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class BallStandBy : MonoBehaviour {
+
+    private delegate void EventHandler(object sender, EventArgs e);
+
+    private BallMove02 ballMove02;
+
+    void Awake() {
+
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit) {
+        if (hit.gameObject.tag == "Player")
+        {
+        }
+    }
+}
